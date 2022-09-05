9a0adbcf20dbe602714ebb427c0a87799e0e1eed
elmellamo <th6986@naver.com>
2022-09-05 23:46:19 +0900

    오류 수정 완료

--- app/src/main/java/com/example/dodamdodam/SignUpActivity.java
+++ app/src/main/java/com/example/dodamdodam/SignUpActivity.java
@@ -104,7 +104,9 @@ public class SignUpActivity extends AppCompatActivity {
         builder.setNegativeButton("네", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                finish();
+                moveTaskToBack(true); // 태스크를 백그라운드로 이동
+                finishAndRemoveTask(); // 액티비티 종료 + 태스크 리스트에서 지우기
+                android.os.Process.killProcess(android.os.Process.myPid()); // 앱 프로세스 종료
             }
         });
         builder.show();
