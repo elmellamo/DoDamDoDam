833e0ed7cc33b784be2cde6ea309649a3dad9442
mywoospam <dobbydavid1@naver.com>
2023-01-22 23:28:40 +0900

    realtime database Calendar - YYMMDD - loveruid - contents 올리고 읽음

--- app/src/main/java/com/example/dodamdodam/activity/Calendar/CalendarMain.java
+++ app/src/main/java/com/example/dodamdodam/activity/Calendar/CalendarMain.java
@@ -1,12 +1,14 @@
 package com.example.dodamdodam.activity.Calendar;
 
+import static androidx.fragment.app.FragmentManager.TAG;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 import com.example.dodamdodam.R;
 import com.example.dodamdodam.activity.Login.BasicActivity;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -14,14 +16,11 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import android.annotation.SuppressLint;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
@@ -29,9 +28,7 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.Calendar;
+
 
 public class CalendarMain extends BasicActivity {
     public String readDay = null;
@@ -43,6 +40,10 @@ public class CalendarMain extends BasicActivity {
     private String stringDateSelected;
     private DatabaseReference databaseReference;
     private FirebaseUser user;
+    private DatabaseReference userdatabaseReference;
+    private DatabaseReference loveruidReference;
+    private DatabaseReference loveruidcodeReference;
+    private String LOVERUID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -56,7 +57,33 @@ public class CalendarMain extends BasicActivity {
         cha_Btn = findViewById(R.id.cha_Btn);
         todayText = findViewById(R.id.todaytext);
         contextEditText = findViewById(R.id.contextEditText);
+        user = FirebaseAuth.getInstance().getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference("Calendar");
+        userdatabaseReference = FirebaseDatabase.getInstance().getReference("users");
+        loveruidReference = userdatabaseReference.child(user.getUid());
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //LOVERUID = db.collection("users").document(user.getUid()).collection("lover").get().toString();
+
+
+        DocumentReference docRef = db.collection("users").document(user.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        LOVERUID = document.getData().get("lover").toString();
+                    } else {
+                        Log.d(TAG, "없다없다없다없다");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
         {
@@ -67,8 +94,8 @@ public class CalendarMain extends BasicActivity {
                 databaseReference.child(stringDateSelected).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.getValue()!=null){
-                            todayText.setText(snapshot.getValue().toString());
+                        if(snapshot.child(LOVERUID).getValue()!=null){
+                            todayText.setText(snapshot.child(LOVERUID).getValue().toString());
                             diaryTextView.setVisibility(View.VISIBLE);
                             save_Btn.setVisibility(View.INVISIBLE);
                             contextEditText.setVisibility(View.INVISIBLE);
@@ -87,7 +114,6 @@ public class CalendarMain extends BasicActivity {
                             cha_Btn.setVisibility(View.INVISIBLE);
                             del_Btn.setVisibility(View.INVISIBLE);
                             diaryTextView.setText(String.format("%d / %d / %d", year, month + 1, dayOfMonth));
-
                         }
                     }
 
@@ -113,7 +139,7 @@ public class CalendarMain extends BasicActivity {
                 contextEditText.setVisibility(View.INVISIBLE);
                 todayText.setVisibility(View.VISIBLE);
                 todayText.setMovementMethod(new ScrollingMovementMethod());
-                databaseReference.child(stringDateSelected).setValue(contextEditText.getText().toString());
+                databaseReference.child(stringDateSelected).child(LOVERUID).setValue(contextEditText.getText().toString());
             }
         });
 
@@ -139,12 +165,12 @@ public class CalendarMain extends BasicActivity {
             public void onClick(View view)
             {
                 todayText.setVisibility(View.INVISIBLE);
-                contextEditText.setText("");
+                contextEditText.setText(null);
                 contextEditText.setVisibility(View.VISIBLE);
                 save_Btn.setVisibility(View.VISIBLE);
                 cha_Btn.setVisibility(View.INVISIBLE);
                 del_Btn.setVisibility(View.INVISIBLE);
-                databaseReference.child(stringDateSelected).setValue(null);
+                databaseReference.child(stringDateSelected).child(LOVERUID).setValue(null);
             }
         });
 
