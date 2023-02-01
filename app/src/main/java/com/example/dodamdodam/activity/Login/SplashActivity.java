52d96f2ef406e87fce681b60ca876ac75b048f62
mywoospam <dobbydavid1@naver.com>
2023-02-01 17:05:33 +0900

    로딩화면 gif

---
+++ app/src/main/java/com/example/dodamdodam/activity/Login/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.example.dodamdodam.activity.Login;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.dodamdodam.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanseState){
+        super.onCreate(savedInstanseState);
+        setContentView(R.layout.activity_splash);
+
+
+
+        ImageView gif_image = (ImageView) findViewById(R.id.gif_image);
+        Glide.with(this).load(R.drawable.dodamrabbit).into(gif_image);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(main);
+                finish();
+            }
+        },3500);
+    }
+
+
+}
